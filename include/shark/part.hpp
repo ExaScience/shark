8328427bdd9b6bc0758d4941c5fef8abaff13737
Bruno De Fraine <brunodf@lab.exascience.com>
2012-10-04 14:39:59 +0200

    More particles

---
+++ include/shark/part.hpp
@@ -0,0 +1,20 @@
+#ifndef __SHARK_PART_HPP
+#define __SHARK_PART_HPP
+
+#include "vec.hpp"
+
+namespace shark {
+	
+	namespace ndim {
+
+		template<int ndim, typename T>
+		struct part {
+			vec<ndim,T> x;
+			vec<ndim,T> v;
+		};
+
+	}
+
+}
+
+#endif
