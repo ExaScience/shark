4b6f787b361bc0dc4897d03d282d62262fb86d6b
Tom Vander Aa <vanderaa@imec.be>
2014-07-23 14:02:04 +0200

    shark: added test for file-io

---
+++ dump_to_file.cpp
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2010-2013, Vrije Universiteit Brussel.
+ * All rights reserved.
+ */
+
+#include <cmath>
+#include <iostream>
+#include <sstream>
+#include <getopt.h>
+#include <shark.hpp>
+
+using namespace std;
+using namespace shark;
+using namespace shark::types1d;
+
+
+void test_dump() {
+	int n = 511;
+
+	Domain d(world(), {{n}});
+	auto src = coord_val<0>(d);
+	GlobalArrayI ga(d);
+	ga = src;
+	ga.dump(ga.region(), "ga.dump");
+}
+
+int main(int argc, char **argv) {
+	Init(&argc, &argv);
+	SetupThreads();
+
+	test_dump();
+
+	Finalize();
+
+	return 0;
+}
