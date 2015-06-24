37ae1de45a68f695a3fb3a87058c34c87f7b1e71
Tom Vander Aa <vanderaa@imec.be>
2015-06-24 14:00:56 +0200

    shark: pretty print size

--- tests/heat3d.cpp
+++ tests/heat3d.cpp
@@ -15,6 +15,28 @@ using namespace std;
 using namespace shark;
 using namespace shark::types3d;
 
+double roundOff(double n) {
+    double d = n * 100.0;
+    int i = d + 0.5;
+    d = (float)i / 100.0;
+    return d;
+}
+
+std::pair<double, const char *> convertSize(size_t size) {               
+    static const char *SIZES[] = { "B", "KB", "MB", "GB" };
+    int div = 0;
+    size_t rem = 0;
+
+    while (size >= 1024 && div < (sizeof SIZES / sizeof *SIZES)) {
+        rem = (size % 1024);
+        div++;
+        size /= 1024;
+    }
+
+    double size_d = (double)size + (double)rem / 1024.0;
+    return make_pair(size_d, SIZES[div]);
+}
+
 void init_pyramid(GlobalArrayD& ga)
 {
 	const coords_range outer = { ga.domain().total().lower - ga.ghost_width(), ga.domain().total().upper + ga.ghost_width() };
@@ -138,7 +160,8 @@ int main(int argc, char **argv)
 	if(world().procid == 0)
 	{
 		cerr << "sched: " << sched << endl;
-		cerr << "n: " << n  << " ( " << n*n*n*sizeof(double)/1e9 << "GB)" << endl;
+                auto s = convertSize(n*n*n*sizeof(double));
+		cerr << "n: " << n  << " (" << s.first << s.second << ")" << endl;
 		cerr << "niter: " << iter << endl;
 		cerr << "block: " << boolalpha << block << noboolalpha << endl;
 		cerr << "nprocs: " << world().nprocs << endl;
